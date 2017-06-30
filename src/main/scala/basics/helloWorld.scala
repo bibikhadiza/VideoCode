9f88fd8187efaa3b8fa6bd36c691bbe89d5da9c8
bibi khadiza <limakhadiza@yahoo.com>
2017-06-30 17:10:40 -0400

    scopes and scala packages

--- src/main/scala/basics/helloWorld.scala
+++ src/main/scala/basics/helloWorld.scala
@@ -1,6 +1,8 @@
+package basics
+
 object HelloWorld {
 
-  val name:String =  "Pat cook"
+  val name: String = "Pat cook"
   val value: Int = 42
   var age = 22
   println(age += 1)
@@ -28,20 +30,20 @@ object HelloWorld {
 
   def main(args: Array[String]): Unit = {
     println("Hello World")
-  }
 
-  /*This file created for Object0orientation, abstraction, and data structures using scala*/
 
-  value.+(age) == value + age
+    /*This file created for Object0orientation, abstraction, and data structures using scala*/
 
-  /* expression are literals that can be combiened with operators to execute value
+    value.+(age) == value + age
+
+    /* expression are literals that can be combiened with operators to execute value
    in scala the symbols are not built into the landguage its implimented in a library
   * in scala symbols have higher precidence then characters
   * */
 
-  name == "Pat cook" * 5
+    name == "Pat cook" * 5
 
-  /* Lamda expressions/ closures
+    /* Lamda expressions/ closures
   * it is a short literal expression that defines a function
   * val square = (x: Double) =>  x * x
   * short literal expressio nthat defines a function/ should not be overly long. simple as possible
@@ -52,33 +54,31 @@ object HelloWorld {
   * */
 
 
+    var i = 0 //in while loops you should use the var eyework beacaucse var is mutable
 
-  var i = 0  //in while loops you should use the var eyework beacaucse var is mutable
+    while (i < 10) {
+      println(i)
+      i += 1
+    }
 
-  while (i < 10){
-    println(i)
-    i += 1
-  }
+    /*In if else conditions if the true and false values within the condition are not the same type, the return value of the function is a type any */
 
-  /*In if else conditions if the true and false values within the condition are not the same type, the return value of the function is a type any */
+    // for loops / comprehensions
 
-  // for loops / comprehensions
+    val stuff = for {
+      i <- 0 until 10
+      if i % 3 == 0 || i % 5 == 0
+      sqr = i * i
+      j <- 'a' to 'c'
+    } yield {
+      i -> j
+    }
 
-  val stuff = for {
-    i <- 0 until 10
-    if i%3 == 0 || i%5 == 0
-    sqr = i * i
-    j <- 'a' to 'c'
-  } yield
-   {
-    i -> j
-  }
+    println(stuff)
 
-  println(stuff)
+    //for loops in scala consists of a generator, conditionals
 
-  //for loops in scala consists of a generator, conditionals
-
-  /* Scala match is powerful because it gives you the availability to do pattern matching
+    /* Scala match is powerful because it gives you the availability to do pattern matching
   With matches the first parameter of the match is the truthy conditions
   and match function then takes in a block parametere that checks to determine
   if any cases match. We do this by calling a case function and passing alone the projected outcome
@@ -86,16 +86,16 @@ object HelloWorld {
   else we have a fine case statment that takes in anything and returns it to a string in my case.
   * */
 
-  var fizzbuzz = for(i <- 1 to 20) yield {
-    (i%3, i%5) match {
-      case (0, 0) if i < 5=> "fizzbuzz"
-      case (0, _) => "fizz"
-      case (_, 0) => "buzz"
-      case _ => i.toString
+    var fizzbuzz = for (i <- 1 to 20) yield {
+      (i % 3, i % 5) match {
+        case (0, 0) if i < 5 => "fizzbuzz"
+        case (0, _) => "fizz"
+        case (_, 0) => "buzz"
+        case _ => i.toString
+      }
     }
-  }
 
-  /* Catch / try is used to catch errors
+    /* Catch / try is used to catch errors
   in a catch method you want to be able to catch any errors what might be thrown when your equation is not executable.
   When creating a try expression we block out our operation within a try function.
   the catch function takes the try function and evaluates if the equation returns an error
@@ -104,24 +104,40 @@ object HelloWorld {
   In catches we have to set cases for all error catches in order to make sure all error expections are caught.
   * */
 
-  var stri = "123a"
-  val num = try {
-    stri.toInt
-  } catch {
-    case ex: NumberFormatException => 0
+    var stri = "123a"
+    val num = try {
+      stri.toInt
+    } catch {
+      case ex: NumberFormatException => 0
+
+    }
+    println(num)
+
+
+    /*SCOPES*/
+
+    /*Scopes in scala are encapsulated within blocks
+  * declaration in scala: val, var, object, def
+  * key work of the declaration followed by the name
+  * After you declare something it can only live in a certain section in your program
+  * it lives from point of declaration until the end of the curly braces that it lives inside of
+  * arguments can only be used within the given method
+  * */
+
+
+
+
+
+
+
+
+
+
+
+
+
 
   }
-  println(num)
-
-
-
-
-
-
-
-
-
-
 }
 
 
