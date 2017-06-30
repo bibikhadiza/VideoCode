010cf5076fd674ebe4ba531174467ee4ddb8ebdc
bibi khadiza <limakhadiza@yahoo.com>
2017-06-30 12:45:13 -0400

    string interpolation in scala

--- src/main/scala/helloWorld.scala
+++ src/main/scala/helloWorld.scala
@@ -15,10 +15,13 @@ object HelloWorld {
   //types: String, Int, Double, Char, Boolean, Unit(type that doesnt represent any information)
 
   val t = (1, 2.7, "hi there")
+  val (a, b, c) = t
+  println(t._1)
 
-
-
-
+  val message = name + " is " + age + "years old."
+  val message2 = s"$name is $age years old."
+  val str = s"The second element of t is ${t._2}. "
+  println(str)
 
   def main(args: Array[String]): Unit = {
     println("Hello World")
