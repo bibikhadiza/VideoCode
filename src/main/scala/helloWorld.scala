eaa8cef6a82a40cd3182850f56fc95bb85d60c82
bibi khadiza <limakhadiza@yahoo.com>
2017-06-30 12:37:58 -0400

    varibles and types in scala

--- src/main/scala/helloWorld.scala
+++ src/main/scala/helloWorld.scala
@@ -1,4 +1,25 @@
 object HelloWorld {
+
+  val name:String =  "Pat cook"
+  val value: Int = 42
+  var age = 22
+  println(age += 1)
+
+  //Difference between val and var
+  // Beacause age is var declaration it can be reassigned to point to a different object
+  //val is a final declaration. It cannot be changed. Immutable
+  //val will always point to one object and will always point to the same object
+
+  //val is prefered because it simplies the logic. Its easier to think about in programs
+
+  //types: String, Int, Double, Char, Boolean, Unit(type that doesnt represent any information)
+
+  val t = (1, 2.7, "hi there")
+
+
+
+
+
   def main(args: Array[String]): Unit = {
     println("Hello World")
   }
