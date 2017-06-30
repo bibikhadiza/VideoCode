425bb47545b41422e876f3db4a4c5f510a30a1ea
bibi khadiza <limakhadiza@yahoo.com>
2017-06-30 12:25:39 -0400

    created object to print CL scala strings

---
+++ src/main/scala/helloWorld.scala
@@ -0,0 +1,9 @@
+object HelloWorld {
+  def main(args: Array[String]): Unit = {
+    println("Hello World")
+  }
+
+  /*This file created for Object0orientation, abstraction, and data structures using scala*/
+}
+
+
