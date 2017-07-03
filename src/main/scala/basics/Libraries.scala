2616f0766812edc971407dd2f57d41077fedce33
bibi khadiza <limakhadiza@yahoo.com>
2017-07-03 10:06:58 -0400

    new ideas

--- src/main/scala/basics/Libraries.scala
+++ src/main/scala/basics/Libraries.scala
@@ -13,11 +13,11 @@ object Libraries {
     val name = readLine()
     println("How old are you?")
     val age = readInt()
-  }
 
-  /*SCALA ARRAYS AND LISTS*/
 
-  /*They are both created almost equally
+    /*SCALA ARRAYS AND LISTS*/
+
+    /*They are both created almost equally
   * The difference is that one is immutable and one if mutable
   * array is mutable and the list is not
   * val cannot be changed
@@ -31,4 +31,31 @@ object Libraries {
   * list.toArray => convert list to array
   * array.toList => converts array to list
   * */
+
+  /*Fill and Tabulate*/
+
+  /* array and lists cannot create large collections and are not ideal for these practices
+  fill is a curried method
+  * */
+
+  /*RANGE*/
+  /* Range provides and range of value from the defined value to the parameter passed to the to function
+  for example 1 to 10 is equavlent to 1.to(10)
+  this will bring a range of integers within the range on 1 and 10
+  you can also set conditions to your ranges
+  1 to 10 by 2 is equalivalent to the executable 1.to(10).by(2)
+  However although both characters and integers are able to provide ranges
+  a type Double cannot provide a range unless appended with the by function
+  this is due to the fact that Doubles can be ranged by any double integer itself.
+  10.0.to(20.0).by(0.1)
+  
+  *
+  *
+  *
+  *
+  *
+  * */
+
+
+  }
 }
