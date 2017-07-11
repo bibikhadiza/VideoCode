86d37cc6535abb69bd23c270e4b0d1d372c3c30f
bibi khadiza <limakhadiza@yahoo.com>
2017-07-11 15:25:24 -0400

    account class, instatance methods and private variables

--- src/main/scala/oobasics/oobasics/bank/Account.scala
+++ src/main/scala/oobasics/oobasics/bank/Account.scala
@@ -3,6 +3,43 @@ package oobasics.oobasics.bank
 /**
   * Created by bibikhadiza on 7/5/17.
   */
-class Account {
+class Account(customer: Customer, val id: String) {
+  private var _balance = 0
+
+  def balance = _balance
+
+  def deposit(amount: Int): Boolean = {
+    if(amount < 0) false
+    else {
+      _balance += amount
+      true
+    }
+  }
+
+  def withdrawl(amount: Int): Boolean = {
+    if(amount < 0 || amount > _balance) false
+    else {
+      _balance -= amount
+      true
+    }
+  }
+
+  /*In the above source code we are constructing a class account
+  class account has many customer and had an id
+  an account object can only be instantiated given an id and a customer
+  If we have available private for public var and val within our class, those values and variables are that are optional and does not need to
+    be required inorder to instantiate an account object.
+
+  */
+
+  /* Private variables can be accessed outside of the class scope
+  but not changed. */
+
+  /* The functions deposit and withdrawl are instant methods that check conditions
+  and based on the conditions either reduce or add to the balance */
+
+
+
+
 
 }
--- src/main/scala/oobasics/oobasics/bank/Customer.scala
+++ src/main/scala/oobasics/oobasics/bank/Customer.scala
@@ -3,6 +3,15 @@ package oobasics.oobasics.bank
 /**
   * Created by bibikhadiza on 7/5/17.
   */
-class Customer {
+class Customer(val firstName: String,
+               val lastName: String,
+               val id: Int,
+               private var _address: Address
+              ){
 
+  private var accounts = List[Account]
 }
+
+
+//Singleton objects cannot be instatitated
+
